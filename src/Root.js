25c36e98c6d8ec82c6d55568096b333df9c67120
unknown <tomasz.garbarz9@gmail.com>
2021-12-16 11:47:32 +0100

    update

--- src/Root.js
+++ src/Root.js
@@ -33,7 +33,7 @@ const Root = () => {
     axios
       .get(
         `
-        https://api.themoviedb.org/3/search/movie?api_key=${api}&language=en-US&query=${inputValue}&page=${page}&include_adult=false`
+        https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${inputValue}&page=${page}&include_adult=false`
       )
       .then((res) => {
         setSearch(res.data.results);
